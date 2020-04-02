483b5f8e23e4266c81be3eddfd7e1c1cbf11e222
hld67890 <hld67890@163.com>
2020-04-02 23:05:17 +0800

    source

---
+++ assignment-1/18307130064/source.py
@@ -0,0 +1,181 @@
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import random
+
+def getdata ():
+    fle = open ( "dataset.data" , "r" )
+    lines = int(fle.readline())
+    #print ( lines )
+    retx = np.empty ( (lines,2) )
+    rety = np.empty ( (lines,1) )
+    for i in range ( lines ):
+        (retx[i][0],retx[i][1],rety[i]) = fle.readline().split()
+    rety=rety.astype ( int )
+    return (lines,retx,rety)
+
+'''
+Generative model
+'''
+
+def cal ( n , x , y ):
+    pi = np.zeros ( (3) )
+    mu = np.zeros ( (3,2) )
+    sigma = np.zeros ( (2,2) )
+    for i in range ( n ):
+        pi[y[i]-1] += 1
+        mu[y[i]-1] += x[i]
+    for i in range ( 3 ):
+        mu[i] /= pi[i]
+        pi[i] /= n
+    for i in range ( n ):
+        sigma += np.dot ( (x[i]-mu[y[i]-1]).transpose() , (x[i]-mu[y[i]-1]) )
+    sigma /= n
+    return (pi,mu,sigma)
+
+def predictgen ( nowx , pi , mu , sigma ):
+    prob = np.zeros ( 3 )
+    sum = 0.0
+    for i in range ( 3 ):
+        prob[i] = stats.multivariate_normal ( mu[i] , sigma ).pdf ( nowx ) * pi[i]
+        sum += prob[i]
+    prob /= sum
+    maxx = -1.0
+    maxi = 0
+    for i in range ( 3 ):
+        if maxx < prob[i]:
+            maxx=prob[i]
+            maxi = i
+    return maxi + 1
+
+
+def testgen ( n , x , y , pi , mu , sigma ):
+    prd = np.zeros ( n )
+    acc = 0
+    for i in range ( n ):
+        prd[i] = predictgen ( x[i] , pi , mu , sigma )
+        if prd[i] == y[i]: acc += 1
+
+    print ( "Generative model" )
+    print ( "Accuracy:" , acc / n , "\n" )
+
+    out = x.transpose ()
+    col = ['ro','bo','go']
+    wrong = ['rx','bx','gx']
+    plt.figure(figsize=(10,10))
+    for i in range ( n ):
+        if y[i][0] == int(prd[i]):
+            plt.plot ( out[0][i] , out[1][i] , col[y[i][0]-1] )
+        else:
+            plt.plot ( out[0][i] , out[1][i] , wrong[y[i][0]-1] )
+    plt.show ()
+
+    print ( "Wrong cases:" )
+    for i in range ( n ):
+        if y[i][0] != int(prd[i]):
+            print ( x[i] , "real:" , y[i][0] , "predict:" , int(prd[i]) )
+    print ()
+
+    return
+
+
+'''
+Discriminative model
+'''
+
+def sigmoid ( x ):
+    return 1.0/(1+np.exp(-x))
+
+def predict_sgd ( j , nowx , w , w0 ):
+    #print ( nowx , w[j] , np.dot ( nowx , w[j] ) )
+    return sigmoid (np.dot ( nowx , w[j] ) + w0[j])
+
+def sgd ( n , x , y , batch , epoch , alpha ):
+    w = np.zeros ( (3,2) )
+    w0 = np.zeros ( (3,1) )
+    prd = np.zeros ( (3,n) ) 
+    dec = alpha / epoch
+
+    zp = list(zip(x,y))
+    x,y = zip (*zp)
+
+    now = 0
+    for ep in range ( epoch ):
+        random.shuffle ( zp )
+        for i in range ( batch ):
+            now += 1
+            if now == n:
+                now = 0
+                break
+            dw = np.zeros ( (3,2) )
+            dw0 = np.zeros ( (3,1) )
+            for j  in range ( 3 ):
+                prd[j][now] = predict_sgd ( j , x[now] , w , w0 )
+                dw[j] += ((1 if y[now]==j+1 else 0)-prd[j][now]) * x[now]
+                dw0[j] += ((1 if y[now]==j+1 else 0)-prd[j][now])
+            #print ( dw )
+            #print ( dw0 )
+            w += dw/batch * alpha
+            w0 += dw0/batch * alpha
+        alpha -= dec
+    return (w,w0)
+
+
+def predictdis ( nowx , w , w0 ):
+    prob = np.zeros ( 3 )
+    sum = 0.0
+    for i in range ( 3 ):
+        prob[i] = sigmoid(np.dot ( w[i] , nowx ) + w0[i])
+        sum += prob[i]
+    prob /= sum
+    maxx = -1.0
+    maxi = 0
+    for i in range ( 3 ):
+        if maxx < prob[i]:
+            maxx=prob[i]
+            maxi = i
+    return maxi + 1
+
+
+def testdis ( n , x , y , w , w0 ):
+    prd = np.zeros ( n )
+    acc = 0
+    for i in range ( n ):
+        prd[i] = predictdis ( x[i] , w , w0 )
+        if prd[i] == y[i]: acc += 1
+
+    print ( "Discriminative model" )
+    print ( "Accuracy:" , acc / n , "\n" )
+
+    out = x.transpose ()
+    col = ['ro','bo','go']
+    wrong = ['rx','bx','gx']
+    plt.figure(figsize=(10,10))
+    for i in range ( n ):
+        if y[i][0] == int(prd[i]):
+            plt.plot ( out[0][i] , out[1][i] , col[y[i][0]-1] )
+        else:
+            plt.plot ( out[0][i] , out[1][i] , wrong[y[i][0]-1] )
+    plt.show ()
+    
+    print ( "Wrong cases:" )
+    for i in range ( n ):
+        if y[i][0] != int(prd[i]):
+            print ( x[i] , "real:" , y[i][0] , "predict:" , int(prd[i]) )
+    print ()
+
+    return
+
+
+# (n,x,y) = getdata ()
+
+# (pi,mu,sigma) = cal ( n , x , y )
+
+# testgen ( n , x , y , pi , mu , sigma )
+
+# epoch = 2000
+# batch = 10
+# alpha = 1e0
+# (w,w0) = sgd ( n , x , y , batch , epoch , alpha )
+
+# testdis ( n , x , y , w , w0 )
